8c2757935691fa8ab21d634249a83391c6449543
ehuntober <ehuntercrypto@gmail.com>
2023-06-04 11:47:11 +0000

    server modified

--- server.js
+++ server.js
@@ -17,6 +17,33 @@ app.get('/blog', (req,res) =>{
 })
 
 
+app.get('/products', async (req,res) =>{
+    try{
+        const product = await Product.find({});
+        res.status(200).json(product)
+
+    }
+    catch(error){
+        res.status(500).json({
+            message: error.message
+        })
+    }
+})
+
+app.get('/products/:id', async (req,res) =>{
+
+    try{
+       
+
+    }
+    catch(error){
+        res.status(500).json({
+            message: error.message
+        })
+    }
+
+})
+
 app.post('/product', async(req,res)=>{
 
     try{
